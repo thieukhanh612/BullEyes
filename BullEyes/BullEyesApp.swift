cd426579ee090b458a05b133b9ab4710097acc2f
thieukhanh612 <66248083+thieukhanh612@users.noreply.github.com>
2021-06-03 20:25:09 +0700

    Initial Commit

---
+++ BullEyes/BullEyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullEyesApp.swift
+//  BullEyes
+//
+//  Created by Khanh Thieu on 03/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct BullEyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
